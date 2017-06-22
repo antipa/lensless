3893e5d8c938e55b5e1a86383ab74ec563535fe0
Nicholas Antipa <naantipa@gmail.com>
2017-06-22 14:29:49 -0700

    Added isotropic TV

--- diffuser_reconstruct_ADMM.m
+++ diffuser_reconstruct_ADMM.m
@@ -1,14 +1,27 @@
 %psf = imread('/Users/nick.antipa/Documents/Diffusers/Lensless/pco_2d_color/psf_med.tif');
 %psfin = load('/Volumes/PhantomData/Diffusers''nstuff/colorPCO_collimated_calibration_far/pinhole_far_green_coherent.mat');
-psf = psfin.imave;
-ds = .5;
-psfd = imresize(double(psf(:,:,1)),ds,'box');
+
+%psf = psfin.imave;
+psf = double(imread('/Users/nick.antipa/Documents/Diffusers/Lensless/tapeycam/tape_40cm_psf.png'));
+ds = 1;
+if ds ~= 1
+    psfd = imresize(double(psf(:,:,1)),ds,'box');
+else
+    psfd = psf;
+end
 h = psfd/norm(psfd(:),2);
 Nx = size(h,2);
 Ny = size(h,1);
-imin = imread('cameraman.tif');
-im = (double(imresize(imin,2*[Ny,Nx])));
-h1 = figure(1),clf
+
+%%
+%imin = imread('cameraman.tif');
+imin = imread('/Users/nick.antipa/Documents/Diffusers/Lensless/tapeycam/tape_40cm_shrey1.png');
+if size(imin) ~= size(psfd)
+    im = (double(imresize(imin,2*[Ny,Nx])));
+else
+    im = double(imin)/255;
+end
+h1 = figure(1),clf;
 
 %define crop and pad operators to handle 2D fft convolution
 pad = @(x)padarray(x,[size(h,1)/2,size(h,2)/2],0,'both');
@@ -27,7 +40,14 @@ At = @(b)real(ifftshift(ifft2(H_conj.*fft2(ifftshift(pad(b))))));
 %y = A(im);
 %yin = imread('/Volumes/PhantomData/Diffusers''nstuff/Processing_results/20170419_152324_AVG_Video_rgb/AVG_Video.png');
 %yin = demosaic(yin,'bggr');
-y = imresize(double(yin(:,:,2)),ds,'box');
+yin = double(imread('/Users/nick.antipa/Documents/Diffusers/Lensless/tapeycam/tape_40cm_shrey1.png'));
+if ds ~= 1
+    y = imresize(double(yin(:,:,2)),ds,'box');
+else
+    y = double(yin)/255;
+end
+
+%%
 
 
 s0 = zeros;
@@ -62,10 +82,15 @@ imagesc(abs(LtL));
 HtH = H.*H_conj;
 imagesc(abs(HtH));
 
-mu1 = 1e-1;
-mu2 = 1;
-mu3 = 4;
-tau = .1;
+mu1 = 0.020972;
+mu2 = 6.815744;
+mu3 = 1.363149;
+tau = .001;
+
+% mu1 = 1e-1;
+% mu2 = 1;
+% mu3 = 4;
+% tau = .1;
 tauinc = 2;
 taudec = tauinc;
 muinc = 10;
@@ -78,7 +103,7 @@ end
 imagesc(abs(Smult))
 colorbar
 % Compute V denominator
-CtC = pad(ones(size(im)/2));
+CtC = pad(ones(size(im)));
 Vmult = 1./(CtC + mu1);
 
 
@@ -95,17 +120,32 @@ alpha3k = zeros(2*Ny,2*Nx);
 alpha3kp = alpha3k;
 Cty = pad(y);
 tk = 1;
+tv_iso = 1;
 %%
 n = 0;
 niter = 500;
 while n<niter
     n = n+1;
     [Lsk1,Lsk2] = L(sk);
-    ukp_1 = soft(Lsk1+alpha2k_1/mu2,tau/mu2);
-    ukp_2 = soft(Lsk2+alpha2k_2/mu2,tau/mu2);
+    uk_1 = Lsk1+alpha2k_1/mu2;
+    uk_2 = Lsk2+alpha2k_2/mu2;
+    if tv_iso
+        mag = sqrt(cat(1,uk_1,zeros(1,size(uk_1,2))).^2 + ...
+        cat(2,uk_2,zeros(size(uk_2,1),1)).^2);
+        magt = soft(mag,tau/mu2);
+        mag(mag==0) = eps;
+        mmult = magt./mag;
+        mmult(mag==0) = 0;
+        ukp_1 = uk_1.*mmult(1:end-1,:);
+        ukp_2 = uk_2.*mmult(:,1:end-1);
+    else
+        ukp_1 = soft(uk_1,tau/mu2);
+        ukp_2 = soft(uk_2,tau/mu2);
+    end
+
     vkp = Vmult.*(mu1*(alpha1k/mu1 + Hfor(sk)) + Cty);
     wkp = max(alpha3k/mu3 + sk,0);
-
+    
     skp = real(ifftshift(ifft2(Smult .* fft2(ifftshift(mu3*(wkp-alpha3k/mu3) + mu2*Ltv(ukp_1 - alpha2k_1/mu2,ukp_2 - alpha2k_2/mu2) + mu1*Hadj(vkp - alpha1k/mu1))))));
     
     t_kp1 = (1+sqrt(1+4*tk^2))/2;
@@ -141,7 +181,7 @@ while n<niter
         axis image
         
         caxis([0 prctile(skp(:),99.9)])
-        axis(ds/.1*[128.5166  356.6414  107.6861  300.1663]);
+       % axis(ds/.1*[128.5166  356.6414  107.6861  300.1663]);
         colorbar
         colormap gray
         drawnow
